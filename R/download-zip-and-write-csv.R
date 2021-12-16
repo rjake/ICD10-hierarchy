b2bd003624361157dcd7867db146c4e70e6975e2
Jake Riley <rjake@sas.upenn.edu>
2021-12-16 13:59:55 -0500

    Add code to download and unzip

--- R/download-zip-and-write-csv.R
+++ R/download-zip-and-write-csv.R
@@ -24,6 +24,56 @@ year_metadata <-
     paste0(
       "y_", map(year_metadata, pluck, "year")
     )
+  )
+
+
+# view as table
+map_dfr(year_metadata, flatten)
+
+# create directory (if it doesn't exist)
+archive <- "file-history"
+dir.create(archive) |> suppressWarnings()
+
+# download files froc CDC
+download_zip_file <- function(x, overwrite = FALSE) {
+  # x = year_metadata$y_2014
+  message("working on ", x$year, ' - - - - - - -') 
+  archive_zip <- paste0(archive, "/cms/", x$year, ".zip")
+  if (!file.exists(archive_zip) | overwrite) {
+    download.file(x$url, destfile = archive_zip, mode = "wb")
+  }
+}
+
+
+# unzip files from CDC
+unzip_file <- function(x, overwrite = FALSE) {
+  message("working on ", x$year, ' - - - - - - -') 
+  new_xml_path <- paste0(archive, "/icd10_", x$year, ".xml")
+  archive_zip <- paste0(archive, "/cms/", x$year, ".zip")
+
+  if (file.exists(new_xml_path) & !overwrite) {
+    return(
+      message("file already exists for", x$year, "use 'overwrite = TRUE'")
+    )
+  }
+
+  unzip(zipfile = archive_zip, files = x$file, exdir = archive)
+  file.rename(file.path(archive, x$file), new_xml_path)
+}
+
+
+# find xml files using 'list = TRUE'
+unzip(zipfile = "file-history/cms/2022.zip", list = TRUE) |> 
+  filter(str_detect(Name, "abular.*xml$"))
+
+# debugonce(unzip_file); unzip_file(x = year_metadata$y_2016)
+
+# download all zip files
+map(year_metadata, possibly(download_zip_file, NULL)); beepr::beep(5)
+
+# unzip all xml files
+map(year_metadata, possibly(unzip_file, NULL)); beepr::beep(5)
+
 
 # parse all xml and write csv files
 map_chr(year_metadata, pluck, "year") |> 
